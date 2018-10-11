f8385c97a7e02aa5016e73dcdcb3987f63a16cae
Jay Berkenbilt <ejb@ql.org>
2018-10-11 09:29:50 -0400

    mingw

--- build-win.bat
+++ build-win.bat
@@ -1,8 +1,15 @@
 @echo on
 @rem Usage: build-win {32|64} {msvc|mingw}
-if %1 == 64 (
-   call "%VS140COMNTOOLS%\..\..\VC\bin\amd64\vcvars64.bat"
+if (%2 == msvc)
+    if %1 == 64 (
+       call "%VS140COMNTOOLS%\..\..\VC\bin\amd64\vcvars64.bat"
+    ) else (
+       call "%VS140COMNTOOLS%\..\..\VC\bin\vcvars32.bat"
+    )
 ) else (
-   call "%VS140COMNTOOLS%\..\..\VC\bin\vcvars32.bat"
+    choco uninstall mingw
+    choco install msys2
+    path C:\tools\msys64\usr\bin;%PATH%
 )
+
 bash ./build %1 %2
