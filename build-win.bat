8c803ea3480080e327eb04ee6c8e32453904e089
Jay Berkenbilt <ejb@ql.org>
2018-10-11 09:56:45 -0400

    mingw

--- build-win.bat
+++ build-win.bat
@@ -1,11 +1,9 @@
 @echo on
 @rem Usage: build-win {32|64} {msvc|mingw}
-if %2 == msvc (
-    if %1 == 64 (
-       call "%VS140COMNTOOLS%\..\..\VC\bin\amd64\vcvars64.bat"
-    ) else (
-       call "%VS140COMNTOOLS%\..\..\VC\bin\vcvars32.bat"
-    )
+if %1 == 64 (
+   call "%VS140COMNTOOLS%\..\..\VC\bin\amd64\vcvars64.bat"
+) else (
+   call "%VS140COMNTOOLS%\..\..\VC\bin\vcvars32.bat"
 )
 
 bash ./build %1 %2
