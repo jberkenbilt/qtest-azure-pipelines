39485785a03827831423359461d685f9e392e460
Jay Berkenbilt <ejb@ql.org>
2019-08-29 22:54:34 -0400

    check environment

--- build.bat
+++ build.bat
@@ -1,4 +1,6 @@
 @echo on
 set
+dir /s "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise"
+dir /s "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise"
 rem choco install msys2
 rem C:\tools\msys64\usr\bin\env.exe MSYSTEM=MINGW64 /bin/bash -l %CD%/build %1
