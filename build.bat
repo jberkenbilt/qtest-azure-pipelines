e216f813d242c57f5de9ed6258bc8fb1209d6316
Jay Berkenbilt <ejb@ql.org>
2019-08-29 23:01:49 -0400

    check environment

--- build.bat
+++ build.bat
@@ -1,6 +1,8 @@
 @echo on
 set
-dir /s "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise"
-dir /s "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise"
-rem choco install msys2
-rem C:\tools\msys64\usr\bin\env.exe MSYSTEM=MINGW64 /bin/bash -l %CD%/build %1
+if %1 == 64 (
+   call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
+) else (
+   call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars32.bat"
+)
+cl /?
